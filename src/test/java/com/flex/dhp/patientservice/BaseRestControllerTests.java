4a842af165f76cf9f4044cc7af4b22a24f250b3d
David Airth <david.airth@slalom.com>
2017-07-11 12:05:22 -0700

    Refactored common test code into base class

--- src/test/java/com/flex/dhp/patientservice/BaseRestControllerTests.java
+++ src/test/java/com/flex/dhp/patientservice/BaseRestControllerTests.java
@@ -1,7 +1,79 @@
 package com.flex.dhp.patientservice;
 
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mock.http.MockHttpOutputMessage;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
 /**
  * Created by david.airth on 7/11/17.
  */
 public class BaseRestControllerTests {
+    protected MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8"));
+
+    protected MockMvc mockMvc;
+
+    protected String firstName = "Jane";
+    protected String lastName = "Doe";
+
+    protected HttpMessageConverter mappingJackson2HttpMessageConverter;
+
+    protected Patient patient;
+
+    protected List<Carecard> carecardList = new ArrayList<>();
+
+    @Autowired
+    protected PatientRepository patientRepository;
+
+    @Autowired
+    protected WebApplicationContext webApplicationContext;
+
+    @Autowired
+    protected CarecardRepository cardcareRepository;
+
+    @Autowired
+    protected void setConverters(HttpMessageConverter<?>[] converters) {
+
+        this.mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream()
+                .filter(hmc -> hmc instanceof MappingJackson2HttpMessageConverter)
+                .findAny()
+                .orElse(null);
+
+        assertNotNull("the JSON message converter must not be null",
+                this.mappingJackson2HttpMessageConverter);
+    }
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
+
+        this.cardcareRepository.deleteAllInBatch();
+        this.patientRepository.deleteAllInBatch();
+
+        this.patient = patientRepository.save(new Patient(firstName, lastName));
+        this.carecardList.add(cardcareRepository.save(new Carecard(patient, "Care Card 1")));
+        this.carecardList.add(cardcareRepository.save(new Carecard(patient, "Care Card 2")));
+    }
+
+    protected String json(Object o) throws IOException {
+        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
+        this.mappingJackson2HttpMessageConverter.write(
+                o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
+        return mockHttpOutputMessage.getBodyAsString();
+    }
 }
--- src/test/java/com/flex/dhp/patientservice/CarecardRestControllerTests.java
+++ src/test/java/com/flex/dhp/patientservice/CarecardRestControllerTests.java
@@ -1,30 +1,16 @@
 package com.flex.dhp.patientservice;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.mock.http.MockHttpOutputMessage;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
 /**
  * Created by david.airth on 7/11/17.
@@ -32,54 +18,7 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 @WebAppConfiguration
-public class CarecardRestControllerTests {
-    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
-            MediaType.APPLICATION_JSON.getSubtype(),
-            Charset.forName("utf8"));
-
-    private MockMvc mockMvc;
-
-    private String firstName = "Jane";
-    private String lastName = "Doe";
-
-    private HttpMessageConverter mappingJackson2HttpMessageConverter;
-
-    private Patient patient;
-
-    private List<Carecard> carecardList = new ArrayList<>();
-
-    @Autowired
-    private PatientRepository patientRepository;
-
-    @Autowired
-    private WebApplicationContext webApplicationContext;
-
-    @Autowired
-    private CarecardRepository cardcareRepository;
-
-    @Autowired
-    void setConverters(HttpMessageConverter<?>[] converters) {
-
-        this.mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream()
-                .filter(hmc -> hmc instanceof MappingJackson2HttpMessageConverter)
-                .findAny()
-                .orElse(null);
-
-        assertNotNull("the JSON message converter must not be null",
-                this.mappingJackson2HttpMessageConverter);
-    }
-
-    @Before
-    public void setup() throws Exception {
-        this.mockMvc = webAppContextSetup(webApplicationContext).build();
-
-        this.cardcareRepository.deleteAllInBatch();
-        this.patientRepository.deleteAllInBatch();
-
-        this.patient = patientRepository.save(new Patient(firstName, lastName));
-        this.carecardList.add(cardcareRepository.save(new Carecard(patient, "Care Card 1")));
-        this.carecardList.add(cardcareRepository.save(new Carecard(patient, "Care Card 2")));
-    }
+public class CarecardRestControllerTests extends BaseRestControllerTests {
 
     @Test
     public void carecardNotFound() throws Exception {
@@ -122,12 +61,5 @@ public class CarecardRestControllerTests {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(contentType))
                 .andExpect(jsonPath("$", hasSize(3)));
-    }
-
-    protected String json(Object o) throws IOException {
-        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
-        this.mappingJackson2HttpMessageConverter.write(
-                o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
-        return mockHttpOutputMessage.getBodyAsString();
     }
 }
